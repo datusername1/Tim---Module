f494ed7e622c0d44397a07cb9291f9a72a400ed5
Yongsoo Lee <yongsoo07@gmail.com>
2018-11-27 11:42:54 -0800

    prune

--- database/seed/seed.js
+++ database/seed/seed.js
@@ -17,7 +17,7 @@ const fakeDataGenerator = (n) => {
       // src.write(`,Ultraboost v${count},18000,https://s3-us-west-1.amazonaws.com/shoeboost/${Math.ceil(Math.random()*36)}.jpg,${Math.ceil(Math.random()*5)}\n`);
     }
   }
-  
+
   src.end();
   console.timeEnd('write time');
 
@@ -28,6 +28,7 @@ const fakeDataGenerator = (n) => {
   .then(() => {
     console.timeEnd('load time');
   });
+  
 };
 
 fakeDataGenerator(100000);
