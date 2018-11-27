61d63f4cfc6a122b0e468cb79fd8dd86c084fc62
Yongsoo Lee <yongsoo07@gmail.com>
2018-11-27 10:32:37 -0800

    removed seed

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const newrelic = require('newrelic');
-const app = require('./index');
-const { env } = require('../env/.env');
-
-const port = env.PORT;
-
-app.listen(port, () => console.log(`listening on ${port}`));
-
-// module.exports = app;?
